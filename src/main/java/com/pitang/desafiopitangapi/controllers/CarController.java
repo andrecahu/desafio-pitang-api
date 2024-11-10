2ab3ad3ce99f8ef17f19c87a069b0840064aba7c
André Cahú <andrecahu28@gmail.com>
2024-11-10 01:06:52 -0300

    feat/2.2: Visualizar Carros - Adicionado a visualizações de carros associados ao usuário logado

--- src/main/java/com/pitang/desafiopitangapi/controllers/CarController.java
+++ src/main/java/com/pitang/desafiopitangapi/controllers/CarController.java
@@ -7,10 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cars")
@@ -22,5 +21,15 @@ public class CarController {
     @PostMapping
     public ResponseEntity<Car> register(@RequestBody Car car, HttpServletRequest request) throws BusinessException {
         return ResponseEntity.status(HttpStatus.CREATED).body(carService.register(car, request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Car>> findAllByLoggedUser(HttpServletRequest request) {
+        return ResponseEntity.ok(carService.findAllByLoggedUser(request));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Car> findById(@PathVariable String id, HttpServletRequest request) throws BusinessException {
+        return ResponseEntity.ok(carService.findByIdAndLoggedUser(id, request));
     }
 }
--- src/main/java/com/pitang/desafiopitangapi/infra/security/SecurityFilter.java
+++ src/main/java/com/pitang/desafiopitangapi/infra/security/SecurityFilter.java
@@ -43,6 +43,8 @@ public class SecurityFilter extends OncePerRequestFilter {
                 var authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
                 var authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
+            }else if(token != null){
+                throw new InvalidTokenException("Unauthorized");
             }
             filterChain.doFilter(request, response);
         }catch (InvalidTokenException e) {
